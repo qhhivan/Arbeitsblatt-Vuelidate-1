e5f6804544d6a5fab1ceb3fc0bf058ed3d0a0e74
qhhivan <orsolic.i02@htlwienwest.at>
2021-06-11 10:52:39 +0200

    Start

---
+++ form-app 1 - Start/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-form>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field v-model="email" label="E-mail"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-btn class="mr-4">submit</v-btn>
+          <v-btn>clear</v-btn>
+        </v-container>
+      </v-form>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    email: '',
+  }),
+};
+</script>
